43ab1dffae5c852a1b79db911f968ec096c88767
Komal Deepak Ghorpade <121026850+komalg2723@users.noreply.github.com>
2023-02-23 22:52:13 +0530

    arranged

---
+++ 2_BackEnd/GoGrocers/src/main/java/com/code/pojos/OrderStatus.java
@@ -0,0 +1,5 @@
+package com.code.pojos;
+
+public enum OrderStatus {
+	PLACED, PACKING, READY, OUT_FOR_DELIVERY, DELIVERED, CANCELLED
+}
---
+++ 2_BackEnd/GoGrocers/src/main/java/com/code/pojos/Role.java
@@ -0,0 +1,6 @@
+package com.code.pojos;
+
+public enum Role {
+
+	ADMIN,EMPLOYEE,CUSTOMER,DELIVERY_PERSON;
+}
